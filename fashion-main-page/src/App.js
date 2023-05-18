73d04bab66194c0556f3a65ca6cafb240e2bdeef
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-05-18 19:33:37 +0300

    creat__header

--- fashion-main-page/src/App.js
+++ fashion-main-page/src/App.js
@@ -1,9 +1,9 @@
-
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, React)</h1>
+      <Header/>
     </div>
   );
 }
---
+++ fashion-main-page/src/components/header/Header.js
@@ -0,0 +1,19 @@
+import './header.css';
+
+
+function Header () {
+    return (
+
+       <div className="header">
+            <div className="container">
+                <div className="header__row">
+                    <div className="header__logo">Logo</div>
+                    <div className="header__nav">NAV</div>
+                </div>
+            </div>
+       </div>
+
+    )
+}
+
+export default Header;
