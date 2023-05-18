d806cfc855bbc130eddbcff57d591fbacf15c52b
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-05-18 23:28:49 +0300

    add__promoImg

--- fashion-main-page/src/components/promo/promo.jsx
+++ fashion-main-page/src/components/promo/promo.jsx
@@ -1,14 +1,32 @@
-import './promo.css'
+import './promo.css';
+import promoImg from './../../img/images/header-img.jpg';
 
 const Promo = () => {
-    return ( <section className="promo">
+    return (<section className="promo">
         <div className="container">
             <div className="promo__content">
-                <div className="promo__text">TEXT</div>
-                <div className="promo__img">IMG</div>
+                <div className="promo__text">
+                    <div className="promo__title">
+                        LET’S
+                        EXPLORE
+                        UNIQUE
+                        CLOTHES.
+                    </div>
+                    <div className="promo__desc">
+                    Live for Influential and Innovative fashion!
+                    </div>
+                    <div className="promo__btn-wrapper">
+                        <a href="#!" className="promo__btn">
+                        Shop Now
+                        </a>
+                    </div>
+                </div>
+                <div className="promo__img">
+                    <img src={promoImg} alt="Promo" />
+                </div>
             </div>
         </div>
-    </section> );
+    </section>);
 }
- 
+
 export default Promo;
