70377aed06f68587a73caace39262e7757751b12
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-05-19 20:58:34 +0300

    creat__section_arrivals

--- fashion-main-page/src/App.jsx
+++ fashion-main-page/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/header/Header";
 import Promo from "./components/promo/promo";
 import Brands from "./components/brands/brands";
+import Arrivals from "./components/arrivals/arrivals";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Promo />
       <Brands />
+      <Arrivals />
     </div>
   );
 }
---
+++ fashion-main-page/src/components/arrivals/arrivals.jsx
@@ -0,0 +1,20 @@
+
+import './arrivals.css'
+
+
+const Arrivals = () => {
+    return ( <section className="arrivals">
+        <div className="container">
+            <div className="arrivals__header">
+                <h2 className="title-2">NEW ARRIVALS</h2>
+            </div>
+            <div className="arrivals__cards">
+                <div>CARD 1</div>
+                <div>CARD 2</div>
+                <div>CARD 3</div>
+            </div>
+        </div>
+    </section> );
+}
+ 
+export default Arrivals;
