0cfb7324278fb9325d755b5d058cbb631d201e62
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-05-20 18:32:44 +0300

    add__card_hover

--- fashion-main-page/src/components/arrivals/arrivals.jsx
+++ fashion-main-page/src/components/arrivals/arrivals.jsx
@@ -10,9 +10,9 @@ const Arrivals = () => {
                 <h2 className="title-2">NEW ARRIVALS</h2>
             </div>
             <div className="arrivals__cards">
-                <div><Card /></div>
-                <div>CARD 2</div>
-                <div>CARD 3</div>
+                <Card />
+                <Card />
+                <Card />
             </div>
         </div>
     </section> );
