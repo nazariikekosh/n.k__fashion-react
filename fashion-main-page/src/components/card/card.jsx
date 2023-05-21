7b4443189735bc0e0c02c1a7446d7fb1c62ddb81
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-05-21 12:38:43 +0300

    add__cardProps

--- fashion-main-page/src/components/card/card.jsx
+++ fashion-main-page/src/components/card/card.jsx
@@ -1,23 +1,25 @@
 import styles from './Card.module.css'
-import cat01Img from './../../img/categories/cat-01.jpg'
+
 import arrowImg from './../../img/icons/arrow.svg'
 
-const Card = () => {
-    return ( <div className={styles.card}>
+const Card = (props) => {
+
+    return (<div className={styles.card}>
         <a href="#!" className={styles.card__link}></a>
-        <img className={styles.card__img } src={cat01Img} alt="Category..." />
+        <img className={styles.card__img} src={props.img} alt='Category...' />
         <div className={styles.card__body}>
-        <div className={styles.card__text}>
-            <div className={styles.card__title}>Hoodies & Sweetshirt</div>
-            <div className={styles.card__muted}>Explore Now!</div>
-        </div>
-        <div className={styles.card__icon}>
-            <img src={arrowImg} alt="Open" />
+            <div className={styles.card__text}>
+                <div className={styles.card__title}>
+                    {props.title}
+                </div>
+                <div className={styles.card__muted}>Explore Now!</div>
+            </div>
+            <div className={styles.card__icon}>
+                <img src={arrowImg} alt="Open" />
+            </div>
         </div>
 
-        </div>
-       
-    </div> );
+    </div>);
 }
- 
+
 export default Card;
